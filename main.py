a8df6c97db5462ec1451294cfbeed9bfff6ea4c5
Maneshna Choudhary <135442163+Maneshna@users.noreply.github.com>
2025-12-23 15:46:55 +0530

    Create main.py
    Add initial main.py with presence counter

---
+++ main.py
@@ -0,0 +1,6 @@
+# Smart Attendance System - Algorithm Prototype
+
+students = ["S101", "S102", "S103", "S104"]
+presence_count = {s: 0 for s in students}
+
+print("Initial presence:", presence_count)
